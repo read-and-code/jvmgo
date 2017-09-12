f83a555703c9e06f540701423a662c70a1651e45
Frederick-S <mao_xiaodan@hotmail.com>
2017-09-12 17:21:47 +0800

    Add add instructions

---
+++ instructions/math_instructions/fadd.go
@@ -0,0 +1,23 @@
+package math_instructions
+
+import (
+	"github.com/Frederick-S/jvmgo/instructions/base_instructions"
+	"github.com/Frederick-S/jvmgo/runtime_data_area"
+)
+
+// fadd
+// Add float.
+// Both value1 and value2 must be of type float.
+// The values are popped from the operand stack and undergo value set conversion, resulting in value1' and value2'.
+// The float result is value1' + value2'. The result is pushed onto the operand stack.
+type FAdd struct {
+	base_instructions.NoOperandsInstruction
+}
+
+func (fAdd *FAdd) Execute(frame *runtime_data_area.Frame) {
+	operandStack := frame.GetOperandStack()
+	floatValue1 := operandStack.PopFloatValue()
+	floatValue2 := operandStack.PopFloatValue()
+
+	operandStack.PushFloatValue(floatValue1 + floatValue2)
+}
