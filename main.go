09ee8024549d470c1cc6a357d5d91fe721f04537
Frederick-S <mao_xiaodan@hotmail.com>
2017-09-22 14:49:58 +0800

    Add CloneTest

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	// cmd := parseCmd()
 	cmd := &Cmd{
 		jrePath:   "/Library/Java/JavaVirtualMachines/jdk1.8.0_92.jdk/Contents/Home/jre",
-		className: "./java/ObjectTest",
+		className: "./java/CloneTest",
 	}
 
 	if cmd.showVersion {
